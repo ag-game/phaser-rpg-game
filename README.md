ad946e4fa0404a07d1f9214b26574bdc709eb525
alvaro <aaruizf@unal.edu.co>
2021-04-26 16:47:56 -0500

    Add basic scene with text, add jest for unit testing

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # phaser-rpg-game
 An RPG game built with the Phaser Javascript library.
+- Open "localhost:8080"
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
+
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
+    }),
+  ],
 };
