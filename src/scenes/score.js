fa66d9094a910baf458d6c13abf467da681b4a6c
alvaro <aaruizf@unal.edu.co>
2021-04-27 18:38:45 -0500

    Add style for leaderboard table in game over scene

--- src/scenes/score.js
+++ src/scenes/score.js
@@ -10,12 +10,11 @@ class Score extends Phaser.Scene {
   create() {
     const { width, height } = this.sys.game.config;
     this.scores = this.sys.game.globals.allScores.result.sort(sortScores);
+
     if (this.scores) {
-      // this.add.text(2, 2, 'SCORES', { fill: '#fff' });
-      // this.add.text(10, 10, `${JSON.stringify(this.scores.result)}`, { fill: '#fff' });
+      // this.add.text(width / 2, 100, 'SCORES', { fill: '#fff' });
       this.tBody = createTable();
-      // console.log(this.scores);
-      addValuesToTable(this.tBody, this.scores);
+      addValuesToTable(this.scores);
     } else {
       this.add.text(0, 0, 'There was an error while trying to get the scores.', { fill: '#fff' });
     }
